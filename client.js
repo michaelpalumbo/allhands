466d4691711cbbfcc9d493f59f37788e88029d95
michaelpalumbo <emailmichaelpalumbo@gmail.com>
2020-05-04 21:31:55 -0400

    updateto client.js

--- client.js
+++ client.js
@@ -1,11 +1,34 @@
 const WebSocket = require('ws');
- 
-const ws = new WebSocket('50.100.31.66:8080');
+const { Client, Server } = require('node-osc');
+
+const ws = new WebSocket('ws://localhost:8081');
  
 ws.on('open', function open() {
-  ws.send('something');
+  // ws.send('something');
 });
  
 ws.on('message', function incoming(data) {
   console.log(data);
+});
+
+ws.on('error', (err) => {
+    console.log(err.message)
+ });
+
+
+
+/// UDP
+// const client = new Client('127.0.0.1', 3333);
+var udpReceive = new Server(7400, '0.0.0.0');
+ 
+udpReceive.on('listening', () => {
+  console.log('OSC Server is listening.')
+})
+ 
+udpReceive.on('message', (msg) => {
+  msg.shift()
+  console.log('data from 7400', msg)
+  let array = [ msg[0], msg[1], msg[2], msg[3], msg[4], msg[5], msg[6] ]
+  
+  ws.send(array)
 });
--- server.js
+++ server.js
@@ -1,13 +1,44 @@
-const ws = require('ws')
-
 const WebSocket = require('ws');
- 
-const wss = new WebSocket.Server({ port: 8080 });
+const { Client, Server } = require('node-osc');
+
+const udpSend = new Client('127.0.0.1', 3333);
+
+
+const wss = new WebSocket.Server({ port: 8081 });
  
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
+    // console.log(message);
+    console.log(message.data)
+    //  msg = JSON.parse(message.data)
+    // let data = message[0]
+    // console.log(message[1])
+    // let data = message.toString()
+    let data = new Array()
+    data.push(message[0])
+    data.push(message[1])
+    data.push(message[2])
+    data.push(message[3])
+    data.push(message[4])
+    data.push(message[5])
+    data.push(message[6])
+    console.log('data from client', data)
+    // data.push(message.index, message.heartSignal, message.bpm, message.bpmDev, message.bvpa, message.gsrChange, message.gsrLoPass)
+    udpSend.send('/list', data, (err) => {
+      if (err) console.error(err);
+      // udpSend.close();
+    });
+
   });
  
-  ws.send('hi erin');
+  ws.send('something');
 });
+
+process.on('uncaughtException', function (err) {
+  console.log(err);
+}); 
+
+
+
+
+
